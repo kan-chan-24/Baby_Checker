639575a203b6d3682fd6f276417ca496ebe199f5
SHUN KANNO <shunplay1793@gmail.com>
2026-04-02 21:32:44 +0900

    HTMLとCSSを一旦リセット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "home#top"
-  get "about" => "home#about"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
