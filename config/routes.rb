211f6ae12e239700ca696456efabd892b49e37e7
SHUN KANNO <shunplay1793@gmail.com>
2026-04-02 22:10:27 +0900

    入力ページを追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/" => "home#top"
+  get "/input" => "home#input"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
